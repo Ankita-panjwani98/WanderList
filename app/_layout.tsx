0fc1f26f571edb48627d793420f2810c7364dbbf
Pritesh Tupe <mail@priteshtupe.com>
2023-05-22 20:37:25 -0400

    Remove unnecessary components, Add sqlite package

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,14 +6,8 @@ import {
 } from "@react-navigation/native";
 import { useFonts } from "expo-font";
 import { SplashScreen, Stack } from "expo-router";
-import { useEffect } from "react";
 import { useColorScheme } from "react-native";
 import SpaceMono from "../assets/fonts/SpaceMono-Regular.ttf";
-
-export {
-  // Catch any errors thrown by the Layout component.
-  ErrorBoundary,
-} from "expo-router";
 
 // eslint-disable-next-line camelcase
 export const unstable_settings = {
@@ -35,21 +29,6 @@ function RootLayoutNav() {
 }
 
 export default function RootLayout() {
-  const [loaded, error] = useFonts({
-    SpaceMono,
-    ...FontAwesome.font,
-  });
-
-  // Expo Router uses Error Boundaries to catch errors in the navigation tree.
-  useEffect(() => {
-    if (error) throw error;
-  }, [error]);
-
-  return (
-    <>
-      {/* Keep the splash screen open until the assets have loaded. In the future, we should just support async font loading with a native version of font-display. */}
-      {!loaded && <SplashScreen />}
-      {loaded && <RootLayoutNav />}
-    </>
-  );
+  const [loaded] = useFonts({ SpaceMono, ...FontAwesome.font });
+  return loaded ? <RootLayoutNav /> : <SplashScreen />;
 }
