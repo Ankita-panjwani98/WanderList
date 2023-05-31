b41ce1cbec92f96279d600ab0ba9ea76966c84ce
Ankita Panjwani <ankitapanjwani98@gmail.com>
2023-05-30 22:28:24 -0400

    basic google map setup added (#22)

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,10 @@
-import { StyleSheet, Text, View } from "react-native";
-import DB from "../../DB";
+import { Dimensions, StyleSheet, View } from "react-native";
+import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
+// import DB from "../../DB";
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
   },
   title: {
     fontSize: 20,
@@ -16,16 +15,36 @@ const styles = StyleSheet.create({
     height: 1,
     width: "80%",
   },
+  map: {
+    width: "100%",
+    height: "100%",
+  },
 });
 
 export default function TabOneScreen() {
   // This is how to read data
   // const bucketList = DB.bucketList;
   // const settings = DB.settings;
+
+  const { width, height } = Dimensions.get("window");
+  const ASPECT_RATIO = width / height;
+  const LATITUDE_DELTA = 0.02;
+  const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+
+  const INITIAL_POSITION = {
+    latitude: 42.9849,
+    longitude: -81.2453,
+    latitudeDelta: LATITUDE_DELTA,
+    longitudeDelta: LONGITUDE_DELTA,
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} />
+      <MapView
+        style={styles.map}
+        provider={PROVIDER_GOOGLE}
+        initialRegion={INITIAL_POSITION}
+      />
     </View>
   );
 }
