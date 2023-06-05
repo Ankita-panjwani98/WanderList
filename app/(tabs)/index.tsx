4afa95cc5a0180201966cea0b793f26e1e27febe
Ankita-panjwani98 <ankitapanjwani98@gmail.com>
2023-06-05 12:41:29 -0400

    #3 Implemented user permission for using maps

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,7 @@
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, Text } from "react-native";
 import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
+import { useEffect, useState } from "react";
+import * as Location from "expo-location";
 import ItemMarker from "../../components/Marker";
 import useDataContext from "../../context/DataContext";
 
@@ -20,54 +22,48 @@ const styles = StyleSheet.create({
     width: "100%",
     height: "100%",
   },
+  errorText: {
+    textAlign: "center",
+    margin: 10,
+    color: "red",
+  },
 });
-type Item = {
-  title: string;
-  location: string;
-  visited: boolean;
-  latLong?: { latitude: number; longitude: number };
-};
-
-const demoWanderList: Item[] = [
-  {
-    title: "Spring Bank Park",
-    location: "London, Canada",
-    visited: true,
-  },
-  {
-    title: "CN Tower",
-    location: "Toronto",
-    visited: false,
-  },
-  {
-    title: "Golden Gate Bridge",
-    location: "California",
-    visited: false,
-  },
-  {
-    title: "Niagara Falls",
-    location: "Niagara Falls",
-    visited: true,
-  },
-  {
-    title: "Canadian War Museum",
-    location: "Ottawa",
-    visited: false,
-  },
-];
 
 export default function MapTab() {
   const { bucketList } = useDataContext();
-
-  const INITIAL_POSITION = {
+  const [error, setError] = useState("");
+  const [latlong, setLatLong] = useState({
     latitude: 42.9877866,
     longitude: -81.2459254,
+  });
+  useEffect(() => {
+    (async () => {
+      // Ask for location permission
+      const { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setError("Permission not granted!!");
+        return;
+      }
+      // Access the current user's location
+      const location = await Location.getCurrentPositionAsync({});
+      const { latitude, longitude } = location.coords;
+      setLatLong({
+        latitude,
+        longitude,
+      });
+    })();
+    setError("");
+  }, []);
+  const INITIAL_POSITION = {
+    latitude: latlong.latitude,
+    longitude: latlong.longitude,
     latitudeDelta: 8,
     longitudeDelta: 8,
   };
 
   return (
     <View style={styles.container}>
+      {error ? <Text style={styles.errorText}>{error}</Text> : ""}
       <MapView
         style={styles.map}
         provider={PROVIDER_GOOGLE}
