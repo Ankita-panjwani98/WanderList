523bbcc1422d477c5ba079b4a27cc5789cb1a23d
Alla Verkhohliadova <a_verkhohliadova@fanshaweonline.ca>
2023-06-05 19:41:17 -0400

    Close #1
    added alert for deleting all item from the list

--- app/(tabs)/listTab.tsx
+++ app/(tabs)/listTab.tsx
@@ -1,4 +1,11 @@
-import { StyleSheet, Text, ScrollView, Button, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  ScrollView,
+  Button,
+  View,
+  Alert,
+} from "react-native";
 import BucketListItem from "../../components/BucketListItem";
 import useDataContext from "../../context/DataContext";
 import BucketList from "../../DB/BucketList";
@@ -18,15 +25,32 @@ const styles = StyleSheet.create({
 export default function ListTab() {
   const { bucketList, setBucketList } = useDataContext();
 
+  const handleClearList = () => {
+    Alert.alert(
+      "Clear List",
+      "Are you sure you want to delete all items from the list?",
+      [
+        {
+          text: "Cancel",
+          style: "cancel",
+        },
+        {
+          text: "Delete All",
+          style: "destructive",
+          onPress: () => {
+            setBucketList(new BucketList([]));
+          },
+        },
+      ]
+    );
+  };
+
   return bucketList.items.length > 0 ? (
     <ScrollView style={styles.container}>
       {bucketList.items.map((item) => (
         <BucketListItem key={item.id} item={item} />
       ))}
-      <Button
-        title="Clear List"
-        onPress={() => setBucketList(new BucketList([]))}
-      />
+      <Button title="Clear List" onPress={handleClearList} />
     </ScrollView>
   ) : (
     <View style={styles.emptyListView}>
