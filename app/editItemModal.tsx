662f65f5253325a83b43aa7f14faef9e2f75c24f
Alla Verkhohliadova <a_verkhohliadova@fanshaweonline.ca>
2023-06-19 17:08:58 -0400

    new fields for item description
    deleting item from edit window, bucketListItem touchable opening edit window

--- app/editItemModal.tsx
+++ app/editItemModal.tsx
@@ -1,16 +1,19 @@
 import { useState } from "react";
 import {
+  Alert,
   StyleSheet,
+  Switch,
   Text,
   TextInput,
   TouchableOpacity,
   View,
-  // Switch,
 } from "react-native";
 import { useLocalSearchParams, useRouter } from "expo-router";
 import { geocodeAsync } from "expo-location";
+import { FontAwesome } from "@expo/vector-icons";
 import useDataContext from "../context/DataContext";
 import BucketList from "../DB/BucketList";
+import Item from "../DB/Item";
 
 const styles = StyleSheet.create({
   container: {
@@ -30,11 +33,34 @@ const styles = StyleSheet.create({
     alignItems: "center",
     marginBottom: 10,
   },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    marginTop: 10,
+  },
+  deleteButton: {
+    marginHorizontal: 10,
+  },
+  bottomButtonContainer: {
+    flexDirection: "row",
+    marginTop: 20,
+    width: "80%",
+  },
   saveButton: {
+    flex: 1,
     backgroundColor: "green",
     padding: 10,
     borderRadius: 5,
-    marginTop: 20,
+    marginRight: 10,
+    alignItems: "center",
+  },
+  cancelButton: {
+    flex: 1,
+    backgroundColor: "orange",
+    padding: 10,
+    borderRadius: 5,
+    marginRight: 10,
+    alignItems: "center",
   },
   buttonText: {
     color: "white",
@@ -57,13 +83,14 @@ export default function EditItemModal() {
   const [error, setError] = useState("");
   const [title, setTitle] = useState(item?.title);
   const [address, setAddress] = useState(item?.address);
+
   // const [coordinates, setCoordinates] = useState(item.coordinates || []);
-  // const [hasVisited, setHasVisited] = useState(item.hasVisited);
-  // const [description, setDescription] = useState(item.description || "");
-  // const [rating, setRating] = useState(item.rating || 0);
-  // const [priority, setPriority] = useState(item.priority || 0);
-  // const [tag, setTag] = useState(item.tag || "");
-  // const [favourite, setFavourite] = useState(item.favourite || false);
+  const [hasVisited, setHasVisited] = useState(item?.hasVisited);
+  const [description, setDescription] = useState(item?.description || "");
+  const [rating, setRating] = useState(item?.rating || undefined);
+  const [priority, setPriority] = useState(item?.priority || undefined);
+  const [tag, setTag] = useState(item?.tag || "");
+  const [favourite, setFavourite] = useState(item?.favourite || false);
 
   const handleSave = () => {
     if (!item) return;
@@ -72,6 +99,16 @@ export default function EditItemModal() {
 
     if (!address) {
       setError("Address input is required!");
+      return;
+    }
+
+    if (rating && rating > 5) {
+      setError("Rating should be set in range (0-5)!");
+      return;
+    }
+
+    if (priority && priority > 3) {
+      setError("Priority should be set in range (0-3)!");
       return;
     }
 
@@ -93,6 +130,12 @@ export default function EditItemModal() {
             address,
             coordinates,
             updateOn: Date.now(),
+            hasVisited,
+            description,
+            rating,
+            priority,
+            tag,
+            favourite,
           },
         };
 
@@ -112,75 +155,121 @@ export default function EditItemModal() {
       });
   };
 
+  const handleDeleteItem = (i: Item) => {
+    Alert.alert(
+      "Delete Item",
+      `Are you sure you want to delete ${i.title} item?`,
+      [
+        {
+          text: "Cancel",
+          style: "cancel",
+        },
+        {
+          text: "Delete",
+          style: "destructive",
+          onPress: () => {
+            const updatedList = bucketList.items.filter((x) => x.id !== i.id);
+            setBucketList(new BucketList(updatedList));
+            router.back();
+          },
+        },
+      ]
+    );
+  };
+
+  const handleCancel = () => {
+    router.back();
+  };
+
   return (
-    <View style={styles.container}>
-      <TextInput
-        style={styles.input}
-        value={title}
-        onChangeText={setTitle}
-        placeholder="Title"
-      />
-      <TextInput
-        style={styles.input}
-        value={address}
-        onChangeText={setAddress}
-        placeholder="Address"
-      />
-
-      {
-        // <View style={styles.switchContainer}>
-        //   <Text>Visited/Opened:</Text>
-        //   <Switch value={hasVisited} onValueChange={setHasVisited} />
-        // </View>
-        //
-        // <TextInput
-        //   style={styles.input}
-        //   value={coordinates.join(", ")}
-        //   onChangeText={(text) =>
-        //     setCoordinates(text.split(",").map((coord) => Number(coord.trim())))
-        //   }
-        //   placeholder="Coordinates (e.g., latitude, longitude)"
-        // />
-        //
-        // <TextInput
-        //   style={styles.input}
-        //   value={description}
-        //   onChangeText={setDescription}
-        //   placeholder="Description"
-        // />
-        // <TextInput
-        //   style={styles.input}
-        //   value={String(rating)}
-        //   onChangeText={(text) => setRating(Number(text))}
-        //   placeholder="Rating (0-5)"
-        //   keyboardType="numeric"
-        // />
-        // <TextInput
-        //   style={styles.input}
-        //   value={String(priority)}
-        //   onChangeText={(text) => setPriority(Number(text))}
-        //   placeholder="Priority (0-3)"
-        //   keyboardType="numeric"
-        // />
-        // <TextInput
-        //   style={styles.input}
-        //   value={tag}
-        //   onChangeText={setTag}
-        //   placeholder="Tag"
-        // />
-        // <View style={styles.switchContainer}>
-        //   <Text>Favourite:</Text>
-        //   <Switch value={favourite} onValueChange={setFavourite} />
-        // </View>
-      }
-
-      <TouchableOpacity style={styles.saveButton} onPress={handleSave}>
-        <Text style={styles.buttonText}>Save</Text>
-      </TouchableOpacity>
-
-      <View style={styles.errorView}>
-        <Text style={{ color: "red" }}> {error} </Text>
+    <>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity
+          style={styles.deleteButton}
+          // onPress={() => handleDeleteItem(item)}
+          onPress={() => handleDeleteItem(item!)}
+        >
+          <FontAwesome
+            size={30}
+            style={{ marginBottom: -3 }}
+            name="trash"
+            color="#db5c40"
+          />
+        </TouchableOpacity>
       </View>
-    </View>
+
+      <View style={styles.container}>
+        <TextInput
+          style={styles.input}
+          value={title}
+          onChangeText={setTitle}
+          placeholder="Title"
+        />
+        <TextInput
+          style={styles.input}
+          value={address}
+          onChangeText={setAddress}
+          placeholder="Address"
+        />
+
+        <View style={styles.switchContainer}>
+          <Text>Opened/Visited:</Text>
+          <Switch value={hasVisited} onValueChange={setHasVisited} />
+        </View>
+
+        {/* <TextInput
+          style={styles.input}
+          value={coordinates.join(", ")}
+          onChangeText={(text) =>
+            setCoordinates(text.split(",").map((coord) => Number(coord.trim())))
+          }
+          placeholder="Coordinates (e.g., latitude, longitude)"
+        /> */}
+
+        <TextInput
+          style={styles.input}
+          value={description}
+          onChangeText={setDescription}
+          placeholder="Description"
+        />
+        <TextInput
+          style={styles.input}
+          value={rating !== undefined ? String(rating) : ""}
+          onChangeText={(text) => setRating(Number(text))}
+          placeholder="Rating (0-5)"
+          keyboardType="numeric"
+        />
+        <TextInput
+          style={styles.input}
+          value={priority !== undefined ? String(priority) : ""}
+          onChangeText={(text) => setPriority(Number(text))}
+          placeholder="Priority (0-3)"
+          keyboardType="numeric"
+        />
+        <TextInput
+          style={styles.input}
+          value={tag}
+          onChangeText={setTag}
+          placeholder="Tag"
+        />
+        <View style={styles.switchContainer}>
+          <Text>Favourite:</Text>
+          <Switch value={favourite} onValueChange={setFavourite} />
+        </View>
+
+        <View style={styles.bottomButtonContainer}>
+          <TouchableOpacity style={styles.cancelButton} onPress={handleCancel}>
+            <Text style={styles.buttonText}>Cancel</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.saveButton} onPress={handleSave}>
+            <Text style={styles.buttonText}>Save</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.errorView}>
+          <Text style={{ color: "red" }}> {error} </Text>
+        </View>
+      </View>
+    </>
   );
 }
