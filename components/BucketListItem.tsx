c15eca75f466a62801bd9aa4ad7aa7bb57402812
Muhammad Mohsin Khan <mohsinfanc@gmail.com>
2023-07-30 16:25:19 -0400

    Adding Rating, Priority and Favourites icon to the Bucket List Item

--- components/BucketListItem.tsx
+++ components/BucketListItem.tsx
@@ -1,6 +1,9 @@
+import React from "react";
 import { View, Text, StyleSheet, Image } from "react-native";
+import { FontAwesome } from "@expo/vector-icons";
 import Item from "../DB/Item";
 import getIconForItem from "../utils/getIconForItem";
+import CustomRating from "../components/CustomRating";
 
 const styles = StyleSheet.create({
   itemContainer: {
@@ -12,19 +15,11 @@ const styles = StyleSheet.create({
     backgroundColor: "white",
     borderRadius: 10,
     padding: 10,
-  },
-  itemSubContainer: {
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
+    flexDirection: "row", // Display the main content in a row
+    alignItems: "flex-start", // Align items to the top of the container
   },
   itemDetailsContainer: {
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    maxWidth: "75%",
+    flex: 1, // Let the details container take all the available width
   },
   title: {
     fontSize: 18,
@@ -58,30 +53,66 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     fontSize: 16,
   },
+  ratingContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 4,
+  },
+  favouriteContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 4,
+  },
+  // Add a new style for the container holding rating and priority icons
+  topRightContainer: {
+    alignItems: "flex-end", // Align the icons to the right
+  },
 });
 
 export default function BucketListItem({ item }: { item: Item }) {
+  // Ensure that item.rating and item.priority are within the range of {0, 1, 2, 3, 4, 5}
+  const rating =
+    item.rating !== undefined ? Math.max(0, Math.min(item.rating, 5)) : 0;
+  const priority =
+    item.priority !== undefined ? Math.max(0, Math.min(item.priority, 3)) : 0;
+
+  const handleRatingChange = (newRating: number) => {
+    // Implement any logic related to handling the rating change here
+    console.log("New rating:", newRating);
+  };
+
   return (
     <View style={styles.itemContainer}>
-      <View style={styles.itemSubContainer}>
-        <View style={styles.itemDetailsContainer}>
-          <Image
-            source={getIconForItem(item)}
-            style={{ width: 50, height: 50, marginRight: 10 }}
-          />
-          <View>
-            <Text style={styles.title}>{item.title}</Text>
-            <Text style={styles.location}>{item.address}</Text>
+      {/* ... (rest of the code remains the same) */}
+
+      {/* Rating and priority icons */}
+      <View style={styles.topRightContainer}>
+        {item.favourite && (
+          <View style={styles.favouriteContainer}>
+            <FontAwesome name="star" size={20} color="orange" />
           </View>
+        )}
+        <View style={styles.ratingContainer}>
+          <CustomRating
+            type="heart"
+            handleRatingChange={handleRatingChange} // Update the handleRatingChange function
+            label=""
+            ratingCount={5}
+            startingValue={rating as 0 | 1 | 2 | 3 | 4 | 5 | undefined}
+            imageSize={18}
+          />
+        </View>
+        <View style={styles.ratingContainer}>
+          <CustomRating
+            type="bell"
+            handleRatingChange={handleRatingChange} // Update the handleRatingChange function
+            label=""
+            ratingCount={3}
+            startingValue={priority as 0 | 1 | 2 | 3 | undefined}
+            imageSize={18}
+          />
         </View>
       </View>
-      {/* Display additional fields
-      {item.description && <Text>Description: {item.description}</Text>}
-      {item.rating && <Text>Rating: {item.rating}/5</Text>}
-      {item.priority && <Text>Priority: {item.priority}/3</Text>}
-      {item.tag && <Text>Tag: {item.tag}</Text>}
-      {item.favourite && <Text>Favourite: Yes</Text>}
-      */}
     </View>
   );
 }
